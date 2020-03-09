3045880ebf52eecb608ffc68318bd8f33f280107
SeoMariko <mariko@SeoMarikonoMacBook-puro.local>
2020-03-09 23:02:43 +0900

    中身のcommit

---
+++ propose/propose/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  propose
+//
+//  Created by SeoMariko on 2020/03/09.
+//  Copyright © 2020年 Kanata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
